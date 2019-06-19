63dbed2f8da3921db9352e401f4a452ac45ae68c
ivandelcz <yermet@gmail.com>
2019-06-19 14:18:29 +0200

    Test 1996 and 2000 leap years

--- leap_year/leap_year.py
+++ leap_year/leap_year.py
@@ -3,3 +3,13 @@
 
 def is_divisible_by_number(year, number):
     return year % number == 0
+
+
+def is_leap_year(year):
+    if is_divisible_by_number(year, 4):
+        if not is_divisible_by_number(year, 100):
+            return True
+        elif is_divisible_by_number(year, 400):
+            return True
+
+    return False
--- leap_year/test_leap_year.py
+++ leap_year/test_leap_year.py
@@ -1,4 +1,4 @@
-from leap_year import is_divisible_by_number
+from leap_year import is_divisible_by_number, is_leap_year
 
 
 def test_1996_is_divisible_by_4():
@@ -11,3 +11,11 @@ def test_1996_is_not_divisible_by_100():
 
 def test_1996_is_divisible_by_400():
     assert not is_divisible_by_number(1996, 400)
+
+
+def test_1996_is_leap_year():
+    assert is_leap_year(1996)
+
+
+def test_2000_is_leap_year():
+    assert is_leap_year(2000)
