967dbf12cd494b3e01cd6a1c2e307bdd8f1935d1
ivandelcz <yermet@gmail.com>
2019-06-19 14:12:35 +0200

    Refactor divisible functions

--- leap_year/leap_year.py
+++ leap_year/leap_year.py
@@ -1,13 +1,5 @@
 #!/usr/bin/env python
 
 
-def is_divisible_by_4(year):
-    return year % 4 == 0
-
-
-def is_divisible_by_100(year):
-    return year % 100 == 0
-
-
-def is_divisible_by_400(year):
-    return year % 400 == 0
+def is_divisible_by_number(year, number):
+    return year % number == 0
--- leap_year/test_leap_year.py
+++ leap_year/test_leap_year.py
@@ -1,13 +1,13 @@
-from leap_year import is_divisible_by_4, is_divisible_by_100, is_divisible_by_400
+from leap_year import is_divisible_by_number
 
 
-def test_1996_is_divisible_by_four():
-    assert is_divisible_by_4(1996)
+def test_1996_is_divisible_by_4():
+    assert is_divisible_by_number(1996, 4)
 
 
 def test_1996_is_not_divisible_by_100():
-    assert not is_divisible_by_100(1996)
+    assert not is_divisible_by_number(1996, 100)
 
 
 def test_1996_is_divisible_by_400():
-    assert not is_divisible_by_400(1996)
+    assert not is_divisible_by_number(1996, 400)
